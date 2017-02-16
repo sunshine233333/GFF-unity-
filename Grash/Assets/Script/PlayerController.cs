a3a627b700a10284927e0c842563ecdbc69bf422
masato12021996 <masato>
2017-02-16 16:13:46 +0900

    プレイヤー簡易作成、ブロックテンプ作成

---
+++ Grash/Assets/Script/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour {
+	// Use this for initialization
+
+	Rigidbody rigitbody;
+
+	void Start () {
+		rigitbody = GetComponent< Rigidbody >();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKey( KeyCode.RightArrow ) ) {
+			rigitbody.AddForce( new Vector3( 0, 0, 10 ) );
+        }
+		if ( Input.GetKey( KeyCode.LeftArrow ) ) {
+			rigitbody.AddForce( new Vector3( 0, 0, -10 ) );
+		}
+
+		if ( Input.GetKeyDown( KeyCode.Space ) ) {
+			rigitbody.AddForce( new Vector3( 0, 200, 0 ) );
+		}
+	}
+}
