d79e4253c8ec3653c42ff494f4a4dfcfbc307501
masato <masato>
2017-02-23 12:44:54 +0900

    ステージセレクト完成 あとは、ステージ３つとステージボタンの作成かな

---
+++ Grash/Assets/Script/StageSelect/StageSelectMain.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelectMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void changeStage( int StageNum ) {
+		switch( StageNum ) {
+			case 0:
+				SceneManager.LoadScene( "Stage1" );
+				break;
+			case 1:
+				SceneManager.LoadScene( "Stage2" );
+				break;
+			case 2:
+				SceneManager.LoadScene( "Stage3" );
+				break;
+		}
+	}
+}
